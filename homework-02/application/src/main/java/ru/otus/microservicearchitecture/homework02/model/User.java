b5b5e111ba4970a3a78085d87c54011ee6ee5d9e
iakonyakina <iakonyakina@domclick.ru>
2021-02-07 21:14:35 +0300

    fixed crud service bugs

--- homework-02/application/src/main/java/ru/otus/microservicearchitecture/homework02/model/User.java
+++ homework-02/application/src/main/java/ru/otus/microservicearchitecture/homework02/model/User.java
@@ -1,7 +1,9 @@
 package ru.otus.microservicearchitecture.homework02.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +14,8 @@ import javax.persistence.Table;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "USERS")
 public class User {
